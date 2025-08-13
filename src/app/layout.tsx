f5a3bb224e000b20a40599ad796980738cacb0ae
samuel-henrique6 <samuellimaarruda@hotmail.com>
2025-08-13 13:38:56 -0400

    feat: implementando o toastify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,8 @@ export default function RootLayout({
                     {children}
                     <Footer />
                 </Container>
+
+                <ToastifyContainer />
             </body>
         </html>
     )
